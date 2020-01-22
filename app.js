3d763273378693cface0411f829e9c57adbec2cc
June Hyok Cho <jcho0627@gmail.com>
2020-01-22 16:36:18 +0100

    initial setup for file upload test

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require("express"),
+  fileUpload = require("express-fileupload"),
+  cors = require("cors"),
+  bodyParser = require("body-parser");
+
+const app = express();
+
+// enable files upload
+app.use(fileUpload({
+  createParentPath: true
+}))
+
+// add other middleware
+app.use(cors());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
+
+// get
+app.get("/", function (req, res) {
+  // res.render(index.html);
+  res.send("Found");
+})
+
+// post to upload
+app.post("/upload-avatar", async (req, res) => {
+  try {
+    if (!req.files) {
+      res.send({
+        status: false,
+        message: "No file uploaded"
+      });
+
+    } else {
+      // use the name of the input field (i.e."avatar") to retrieve the uploaded file)
+      let avatar = req.files.avatar;
+
+      // use the mv() method to place the file in upload directory (i.e. "uploads")
+      avatar.mv("./uploads/" + avatar.name);
+
+      // send response
+      res.send({
+        status: true,
+        message: "File is uploaded",
+        data: {
+          name: avatar.name,
+          mimetype: avatar.mimetype,
+          size: avatar.size
+        }
+      });
+    }
+  } catch (err) {
+    res.status(500).send(err);
+  }
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`App is listening on port : ${port}`);
+})

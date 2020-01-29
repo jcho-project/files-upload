2f1ba387ef156523f3973f8a6259663b8ad6a1a0
June Hyok Cho <jcho0627@gmail.com>
2020-01-29 17:29:31 +0100

    add thead and th from input data

--- creator.js
+++ creator.js
@@ -1,18 +1,40 @@
-let excelTable = document.getElementById("excel_table");
-
 function generateTable() {
+  let excelTable = document.getElementById("excel_table");
+  let thead = document.createElement("thead")
   let rawData = document.getElementById("excel_data").value
-
   let rows = rawData.split("\n");
 
   rows.pop();
 
   for (let i = 0; i < rows.length; i++) {
-    rows[i] = rows[i].split("\t");
-
-    excelTable.append(document.createElement("tr"))
+    rows[i] = rows[i].split(/\s+/)
   }
 
-  console.log(rawData);
-  console.log(rows);
+  console.log(rows)
+
+  excelTable.appendChild(thead)
+
+  for (let k = 0; k < rows[0].length; k++) {
+    let th = document.createElement("th")
+    let headText = document.createTextNode(rows[0][k])
+
+    th.appendChild(headText)
+    thead.appendChild(th)
+  }
+
+  for (let i = 0; i < rows.length; i++) {
+    let tr = document.createElement("tr")
+
+    excelTable.appendChild(tr)
+
+    for (let j = 0; j < rows[i].length; j++) {
+      let cell = document.createElement("td")
+      let cellText = document.createTextNode(rows[j][j])
+
+      cell.appendChild(cellText)
+      tr.appendChild(cell)
+    }
+  }
+
+
 }

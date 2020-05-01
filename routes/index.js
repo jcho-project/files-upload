3012c9e73c9476d600d00ca21be60f34219e0e50
June Hyok Cho <jcho0627@gmail.com>
2020-05-01 19:51:06 +0200

    change to redirect to templates page after upload // decide not to have different upload inputs // require upload file verification in template-routes

--- routes/index.js
+++ routes/index.js
@@ -5,39 +5,4 @@ router.get("/", (req, res) => {
   res.render("index");
 });
 
-// =========================================================================
-// UPLOAD route - upload document template for further processing
-// =========================================================================
-
-router.post("/upload-avatar", async (req, res) => {
-  try {
-    if (!req.files) {
-      res.send({
-        status: false,
-        message: "No file uploaded"
-      });
-
-    } else {
-      // use the name of the input field (i.e."avatar") to retrieve the uploaded file)
-      let avatar = req.files.sampleFile;
-
-      // use the mv() method to place the file in upload directory (i.e. "uploads")
-      avatar.mv("./uploads/" + avatar.name);
-
-      // send response
-      res.send({
-        status: true,
-        message: "File is uploaded",
-        data: {
-          name: avatar.name,
-          mimetype: avatar.mimetype,
-          size: avatar.size
-        }
-      });
-    }
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
-
 module.exports = router;
--- routes/template-routes.js
+++ routes/template-routes.js
@@ -9,6 +9,32 @@ router.get("/", (req, res) => {
   });
 });
 
+// =========================================================================
+// UPLOAD route - upload document template for further processing
+// =========================================================================
 
+router.post("/upload-avatar", async (req, res) => {
+  try {
+    if (!req.files) {
+      res.send({
+        status: false,
+        message: "No file uploaded"
+      });
+
+    } else {
+
+      // use the name of the input field (i.e."avatar") to retrieve the uploaded file)
+      let avatar = req.files.sampleFile;
+
+      // use the mv() method to place the file in upload directory (i.e. "uploads")
+      avatar.mv("./uploads/" + avatar.name);
+
+      // redirect to templates page
+      res.redirect("/templates");
+    }
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
 module.exports = router;

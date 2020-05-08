11643ac595cd5d53c86517432536df5064eae7be
June Hyok Cho <jcho0627@gmail.com>
2020-05-08 21:59:21 +0200

    further investigation required on dir and file being read // seems to be he factory swiss file

--- controller/he-docs.js
+++ controller/he-docs.js
@@ -2,6 +2,8 @@ const xlsx = require("xlsx"),
   workbook = xlsx.readFile("./uploads/he-creator.xlsx");
 
 let first_sheet_name = workbook.SheetNames[0];
+let second_sheet_name = workbook.SheetNames[1]
+let third_sheet_name = workbook.SheetNames[2]
 let address_of_cell = "A1";
 
 let worksheet = workbook.Sheets[first_sheet_name];
@@ -11,4 +13,6 @@ let desired_value = (desired_cell ? desired_cell.v : undefined);
 
 console.log(workbook.__dirname)
 console.log(first_sheet_name);
+console.log(second_sheet_name);
+console.log(third_sheet_name);
 console.log(desired_value);

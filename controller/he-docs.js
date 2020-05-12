f62c37a89ff97eb8361eef85f58100e9b0518d94
June Hyok Cho <jcho0627@gmail.com>
2020-05-12 07:57:43 +0200

    reupload he-creator.xlsx and check file being read // found hidden worksheet in first and second // require delete hidden worksheet

--- controller/he-docs.js
+++ controller/he-docs.js
@@ -1,18 +1,25 @@
 const xlsx = require("xlsx"),
   workbook = xlsx.readFile("./uploads/he-creator.xlsx");
 
+let data = {
+
+}
+
 let first_sheet_name = workbook.SheetNames[0];
 let second_sheet_name = workbook.SheetNames[1]
 let third_sheet_name = workbook.SheetNames[2]
-let address_of_cell = "A1";
+let fourth_sheet_name = workbook.SheetNames[3]
+let fifth_sheet_name = workbook.SheetNames[4]
+let address_of_cell = "D6";
 
-let worksheet = workbook.Sheets[first_sheet_name];
+let worksheet = workbook.Sheets[third_sheet_name];
 let desired_cell = worksheet[address_of_cell];
 
 let desired_value = (desired_cell ? desired_cell.v : undefined);
 
-console.log(workbook.__dirname)
 console.log(first_sheet_name);
 console.log(second_sheet_name);
 console.log(third_sheet_name);
+console.log(fourth_sheet_name);
+console.log(fifth_sheet_name);
 console.log(desired_value);

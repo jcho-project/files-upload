47bef9a803d9daeb56cb068acdcf8613eece28a7
June Hyok Cho <jcho0627@gmail.com>
2020-05-13 07:28:06 +0200

    delete hidden files in he-creator // add check for vba // add sheet to json for further processing // require highligth cell and use vba

--- controller/he-docs.js
+++ controller/he-docs.js
@@ -1,25 +1,24 @@
 const xlsx = require("xlsx"),
   workbook = xlsx.readFile("./uploads/he-creator.xlsx");
 
-let data = {
-
-}
+let data = xlsx.utils.sheet_to_json(workbook.Sheets["HE_Factory_(Swiss)"], { header: 1 })
 
 let first_sheet_name = workbook.SheetNames[0];
-let second_sheet_name = workbook.SheetNames[1]
-let third_sheet_name = workbook.SheetNames[2]
-let fourth_sheet_name = workbook.SheetNames[3]
-let fifth_sheet_name = workbook.SheetNames[4]
 let address_of_cell = "D6";
 
-let worksheet = workbook.Sheets[third_sheet_name];
+let worksheet = workbook.Sheets[first_sheet_name];
 let desired_cell = worksheet[address_of_cell];
 
 let desired_value = (desired_cell ? desired_cell.v : undefined);
 
+function wb_has_macro(wb) {
+  if (!!wb.vbaraw) {
+    console.log("macro available")
+  };
+  // const sheets = wb.SheetNames.map((n) => wb.Sheets[n]);
+  // return sheets.some((ws) => !!ws && ws['!type'] == 'macro');
+}
+
 console.log(first_sheet_name);
-console.log(second_sheet_name);
-console.log(third_sheet_name);
-console.log(fourth_sheet_name);
-console.log(fifth_sheet_name);
 console.log(desired_value);
+console.log(data);
